83a1a3fdef1a49e99eb24d9b966449ebef954df6
VaibhavJoshi10 <vaibhavjoshi0310199659@gmail.com>
2024-07-27 18:42:39 +0530

    Second commit

--- ReversNumber.java
+++ ReversNumber.java
@@ -1,2 +1,20 @@
 public class ReversNumber {
+    public static void reverseArray(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n / 2; i++) {
+            // Swap the elements at the i-th and (n-i-1)-th indices
+            int temp = arr[i];
+            arr[i] = arr[n - i - 1];
+            arr[n - i - 1] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        reverseArray(arr);
+        // Print the reversed array
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
 }
